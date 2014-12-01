329c73cef51ae3538ef11952a32408f7e3dc5d7c
David Ochoa <radikal_david@hotmail.com>
2014-12-01 16:18:50 -0600

    First version
    First version

---
+++ src/Simulador.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Simulador {
+
+	static Scanner sc = new Scanner(System.in);
+	static int tiempoLimite;
+	static int Reloj = 0;
+	static int Delta = TLL(Unif); //Tiempo de llegada de pasajeros
+	static int NPV = 0;
+	static int Cola = 0;
+	
+public static void main(String args[]){
+	System.out.println("Inserta el tiempo limite de ejecución: ");
+	tiempoLimite = sc.nextInt();
+	
+	
+}
+}
